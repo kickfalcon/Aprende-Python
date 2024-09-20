18878d2761bf52f8c41d0049603051c51f000bb0
Edgar Tornez <88979096+kickfalcon@users.noreply.github.com>
2024-09-20 11:12:31 -0600

    Create Frase.py

---
+++ Ciclos/Retos/Frase.py
@@ -0,0 +1,4 @@
+# Solicitar al usuario el ingreso de una frase y de una letra (que puede o no estar en la frase). 
+# Recorrer la frase, carácter a carácter, comparando con la letra buscada. Si el carácter no coincide, 
+# indicar que no hay coincidencia en esa posición (imprimiendo la posición) y continuar. Si se encuentra una coincidencia, 
+# indicar en qué posición se encontró y finalizar la ejecución.
