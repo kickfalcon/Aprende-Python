c4c25987f1ad47ce06490f1f9242588940dbbe04
Edgar Tornez <88979096+kickfalcon@users.noreply.github.com>
2024-09-20 11:06:51 -0600

    Create Menu.py

---
+++ Ciclos/Retos/Menu.py
@@ -0,0 +1,7 @@
+# Mostrar un menú con tres opciones: 
+#    1- comenzar programa 
+#    2- imprimir listado 
+#    3-finalizar programa. 
+# A continuación, el usuario debe poder seleccionar una opción (1, 2 ó 3). Si elige una opción incorrecta, informarle del error. 
+# El menú se debe volver a mostrar luego de ejecutada cada opción, permitiendo volver a elegir. Si elige las opciones 1 ó 2 se imprimirá un texto. 
+# Si elige la opción 3, se interrumpirá la impresión del menú y el programa finalizará.
